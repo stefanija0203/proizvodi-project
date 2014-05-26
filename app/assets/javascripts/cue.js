4a44120f2c334db287237827a01c2eee0432b93d
stefanija0203 <stefanijakicovic@gmail.com>
2014-05-26 16:12:27 +0200

    cue

--- app/assets/javascripts/cue.js
+++ app/assets/javascripts/cue.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage({
-    	onLeave: function(current, next) {
-    		var d = 1000;
-    		var f = 1000;
+	$('#fullpage').fullpage({
+		onLeave: function(current, next) {
+			var d = 800;
+			var f = 1000;
 
-    		// alert("i: " + current + "dir: " + next);
-    		if((current==1 && next==2) || (current==3 && next==2)) {
-    			$('#first').hide();
-    			$('#second').hide();
-    			$('#third').hide();
-    			$('#fourth').hide();
-    			$('#fifth').hide();
+			// alert("i: " + current + "dir: " + next);
+			if((current==1 && next==2) || (current==3 && next==2)) {
+				$('#first').hide();
+				$('#second').hide();
+				$('#third').hide();
+				$('#fourth').hide();
+				$('#fifth').hide();
 				$('#sixth').hide();
 				$('#seventh').hide();
 				$('#eighth').hide();	
-    			
+				
 				$('#first').delay(d).fadeIn(f);
 				$('#second').delay(2*d).fadeIn(f);
 				$('#third').delay(3*d).fadeIn(f);
 				$('#fourth').delay(4*d).fadeIn(f);
-				$('#fifth').delay(5*d).fadeIn(f);
-				$('#sixth').delay(6*d).fadeIn(f);
-				$('#seventh').delay(7*d).fadeIn(f);
-				$('#eighth').delay(8*d).fadeIn(f);
-    		}
-    		if(current == 2) {
-    			$('#first').finish();
+				$('#fifth').delay(4.5*d).fadeIn(f);
+				$('#sixth').delay(5*d).fadeIn(f);
+				$('#seventh').delay(5.5*d).fadeIn(f);
+				$('#eighth').delay(6*d).fadeIn(f);
+			}
+			if(current == 2) {
+				$('#first').finish();
 				$('#second').finish();
 				$('#third').finish();
 				$('#fourth').finish();
@@ -33,11 +33,24 @@ $(document).ready(function() {
 				$('#sixth').finish();
 				$('#seventh').finish();
 				$('#eighth').finish();
-    		}
-    	}
+			}
+			if((current==3 && next==4) || (current==5 && next==4)) {
+				// function ani(){
+				// 	$('.fourth-right').animate({'margin-top': '80px'}, 4000, function(){
+				// 		$('.fourth-right').css('margin-top','10px');
+				// 		ani();
+				// 	});
+				// };
+				// ani();
+
+				// .fourth-right.animate({top: '70px'}, "slow");
+
+				// $('.fourth-right').animate({top: 70px}, 2000, "swing");
+			}
+		}
 	});
 	
 	$('.carousel').carousel({
-		interval: 1000
+		interval: 2000
 	});
 });
