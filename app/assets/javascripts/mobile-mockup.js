ec74f275e3583b5486375282e443153351e33c22
stefanija0203 <stefanijakicovic@gmail.com>
2014-06-10 13:02:17 +0200

    ideus site

---
+++ app/assets/javascripts/mobile-mockup.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		onLeave: function(current, next) {
+			var d = 800;
+			var f = 1000;
+		}
+	});
+
+	
+	// $('.right_nav').on('click', 'a', function() {
+	//     $('.right_nav a.active').removeClass('active');
+	//     $(this).addClass('active');
+	// 	$(this).css("background-color", "#f9423a");
+	// });
+
+
+});
