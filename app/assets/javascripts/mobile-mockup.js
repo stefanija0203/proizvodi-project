6e88bbad19b6a1ed545dd5f167a7cd03e002218c
stefanija0203 <stefanijakicovic@gmail.com>
2014-06-11 08:50:40 +0200

    Improvements and paralax

--- app/assets/javascripts/mobile-mockup.js
+++ app/assets/javascripts/mobile-mockup.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
       // alert("i: " + current + "dir: " + next);
       if((current==2 && next==3) || (current==4 && next==3)) {
         $("#screen-details-1").css({"bottom": "-338px", "clip": "rect(0px, 299px, 503px, 0px)" });
+        $("#screen-details-2").css({"bottom": "-206px", "clip": "rect(0px, 299px, 503px, 0px)" });
         $("#screen-details-1").delay(3000).animate({ bottom : 0},
         {
           duration:5000,
