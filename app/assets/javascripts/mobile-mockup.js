a4de0435c304800171162430cd64838523ee848b
stefanija0203 <stefanijakicovic@gmail.com>
2014-06-16 16:30:21 +0200

    tablet i web - bez tackica

--- app/assets/javascripts/mobile-mockup.js
+++ app/assets/javascripts/mobile-mockup.js
@@ -7,18 +7,18 @@ $(document).ready(function() {
 
       // alert("i: " + current + "dir: " + next);
       if((current==1 && next==2) || (current==3 && next==2)) {
-        $(".points img").hide();
-        // $(".points img:nth-child(n+1)").delay((n+1)*d).fadeIn(f);
-        setTimeout(function() {  
-          $(".points-section1 img").each(function( index ) {
-            $(this).delay((index+1)*d).fadeIn(f);
-            // console.log( index + ": " + $( this ).attr('alt') );
-          }); 
-        }, 1000);
+        // $(".points img").hide();
+        // // $(".points img:nth-child(n+1)").delay((n+1)*d).fadeIn(f);
+        // setTimeout(function() {  
+        //   $(".points-section1 img").each(function( index ) {
+        //     $(this).delay((index+1)*d).fadeIn(f);
+        //     // console.log( index + ": " + $( this ).attr('alt') );
+        //   }); 
+        // }, 1000);
 
       }
       if(current==2) {
-        $(".points-section1 img").finish();
+        // $(".points-section1 img").finish();
       }
 
       if((current==2 && next==3) || (current==4 && next==3)) {
@@ -31,13 +31,13 @@ $(document).ready(function() {
                 $(this).css('clip', 'rect(' +(now+255)+'px , 299px, '+(now+841)+'px, 0px)')
             }
           }, 1000);
-          $(".points img").hide();
-          setTimeout(function() {  
-            $(".points-section3 img").each(function( index ) {
-              $(this).delay((index+1)*d).fadeIn(f);
-              // console.log( index + ": " + $( this ).attr('alt') );
-            });
-          }, 1000);
+          // $(".points img").hide();
+          // setTimeout(function() {  
+          //   $(".points-section3 img").each(function( index ) {
+          //     $(this).delay((index+1)*d).fadeIn(f);
+          //     // console.log( index + ": " + $( this ).attr('alt') );
+          //   });
+          // }, 1000);
         } else {
           $("#screen-details-1").css({"bottom": "-338px", "clip": "rect(0px, 299px, 503px, 0px)" });
           $("#screen-details-1").delay(3000).animate({ bottom : 0},
@@ -47,18 +47,18 @@ $(document).ready(function() {
                 $(this).css('clip', 'rect(' +(now+338)+'px , 299px, '+(now+841)+'px, 0px)')
             }
           }, 1000);
-          $(".points img").hide();
-          setTimeout(function() {  
-            $(".points-section3 img").each(function( index ) {
-              $(this).delay((index+1)*d).fadeIn(f);
-              // console.log( index + ": " + $( this ).attr('alt') );
-            });
-          }, 1000);  
+          // $(".points img").hide();
+          // setTimeout(function() {  
+          //   $(".points-section3 img").each(function( index ) {
+          //     $(this).delay((index+1)*d).fadeIn(f);
+          //     // console.log( index + ": " + $( this ).attr('alt') );
+          //   });
+          // }, 1000);  
         }
       }
       if(current==3) {
         $("#screen-details-1").finish();
-        $(".points-section3 img").finish();
+        // $(".points-section3 img").finish();
       }
 
       if((current==3 && next==4) || (current==5 && next==4)) {
@@ -87,13 +87,13 @@ $(document).ready(function() {
                 $(this).css('clip', 'rect(' +(now+150)+'px , 299px, '+(now+709)+'px, 0px)')
             }
           }, 1000); 
-          $(".points img").hide();
-          setTimeout(function() {  
-            $(".points-section4 img").each(function( index ) {
-              $(this).delay((index+1)*d).fadeIn(f);
-              // console.log( index + ": " + $( this ).attr('alt') );
-            });
-          }, 1000);
+          // $(".points img").hide();
+          // setTimeout(function() {  
+          //   $(".points-section4 img").each(function( index ) {
+          //     $(this).delay((index+1)*d).fadeIn(f);
+          //     // console.log( index + ": " + $( this ).attr('alt') );
+          //   });
+          // }, 1000);
         } else {
           $("#screen-details-2").css({"bottom": "-206px", "clip": "rect(0px, 299px, 503px, 0px)" });
           $("#screen-details-2").delay(3000).animate({ bottom : 0},
@@ -103,34 +103,34 @@ $(document).ready(function() {
                 $(this).css('clip', 'rect(' +(now+206)+'px , 299px, '+(now+709)+'px, 0px)')
             }
           }, 1000); 
-          $(".points img").hide();
-          setTimeout(function() {  
-            $(".points-section4 img").each(function( index ) {
-              $(this).delay((index+1)*d).fadeIn(f);
-              // console.log( index + ": " + $( this ).attr('alt') );
-            });
-          }, 1000);
+          // $(".points img").hide();
+          // setTimeout(function() {  
+          //   $(".points-section4 img").each(function( index ) {
+          //     $(this).delay((index+1)*d).fadeIn(f);
+          //     // console.log( index + ": " + $( this ).attr('alt') );
+          //   });
+          // }, 1000);
         }
       }
 
       if(current==5) {
         $("#screen-details-2").finish();
-        $(".points-section4 img").finish();
+        // $(".points-section4 img").finish();
       }  
       if(current==5 && next==6) {
-        $(".points img").hide();
-        $(".fa").hide();
-         setTimeout(function() {  
-          $(".points-section5 img").each(function( index ) {
-            $(this).delay((index+1)*d).fadeIn(f);
-            // console.log( index + ": " + $( this ).attr('alt') );
-          });
-          $(".fa").show();
-        }, 1000);
+        // $(".points img").hide();
+        // $(".fa").hide();
+        //  setTimeout(function() {  
+        //   $(".points-section5 img").each(function( index ) {
+        //     $(this).delay((index+1)*d).fadeIn(f);
+        //     // console.log( index + ": " + $( this ).attr('alt') );
+        //   });
+        //   $(".fa").show();
+        // }, 1000);
         
       } 
       if(current==6) {
-        $(".points-section5 img").finish(); 
+        // $(".points-section5 img").finish(); 
       }
 		}
 	});
